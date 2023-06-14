52daf2f95ba740e5f272c5c0553e1eafca7252ab
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-14 09:54:53 +0800

    2306140954

--- JWJava/src/tw/org/iii/jwapp/JW11.java
+++ JWJava/src/tw/org/iii/jwapp/JW11.java
@@ -4,11 +4,13 @@ public class JW11 {
 
 	public static void main(String[] args) {
 		int a = 10;
+//		int a = 100, b = 10;	//常數不行處理
+		final int b = 10;	//需要透過final int 進行定義
 		switch (a) {
 			case 1:
 				System.out.println("A");
 				break;
-			case 10:
+			case b:
 				System.out.println("B");
 //				break;	//break拿掉 如果case在之前驗證，那沒有break就會連同下一個break之前結果同時輸出
 			default:
