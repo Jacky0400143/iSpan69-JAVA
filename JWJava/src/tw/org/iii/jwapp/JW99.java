c30b80edcde4bc7c3d7714328427d35fb8c98937
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-14 10:58:11 +0800

    2306141058

---
+++ JWJava/src/tw/org/iii/jwapp/JW99.java
@@ -0,0 +1,22 @@
+package tw.org.iii.jwapp;
+
+public class JW99 {
+
+	public static void main(String[] args) {
+		for (int i =2; i<=5; i++) {
+			int r = i *1;
+			System.out.printf("%d x 1 = %d\t", i, r);
+	
+		}
+
+	}
+}
+	
+//	public static void main(String[] args) {
+//		System.out.println("2 x 1 = 2");
+//		System.out.println("3 x 1 = 2");
+//		System.out.println("4 x 1 = 2");
+//		System.out.println("5 x 1 = 2");
+//	}
+//
+//}
