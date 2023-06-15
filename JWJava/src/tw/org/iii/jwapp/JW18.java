cc051d81d624957f049094bdd81bfdd25d943eed
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-15 09:19:18 +0800

    2306150919

---
+++ JWJava/src/tw/org/iii/jwapp/JW18.java
@@ -0,0 +1,13 @@
+package tw.org.iii.jwapp;
+
+public class JW18 {
+
+	public static void main(String[] args) {
+		int i = 1, sum = 0;
+		while ( i<= 10) {
+			sum += i++;
+		}
+		System.out.println(sum);
+	}
+
+}
