26e1daa0ba254e9ee64e458f19d5a2b9e18585d4
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-15 09:32:08 +0800

    2306150934

--- JWJava/src/tw/org/iii/jwapp/JW18.java
+++ JWJava/src/tw/org/iii/jwapp/JW18.java
@@ -1,13 +1,20 @@
 package tw.org.iii.jwapp;
 
+import java.util.Scanner;
+
 public class JW18 {
 
 	public static void main(String[] args) {
+		// 1 + 2 + 3 + ... + n = ?
+		System.out.print("n = ");
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
 		int i = 1, sum = 0;
 		while ( i<= 10) {
 			sum += i++;
 		}
-		System.out.println(sum);
+		System.out.printf("1 + 2 + 3 + ... + %d = %d", n, sum);
 	}
 
 }
--- JWJava/src/tw/org/iii/jwapp/PokerV1.java
+++ JWJava/src/tw/org/iii/jwapp/PokerV1.java
@@ -1,10 +1,36 @@
 package tw.org.iii.jwapp;
 
 public class PokerV1 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		long start = System.currentTimeMillis();
+		System.out.println(start);
+		// 1. 洗牌
+		int[] poker = new int[52];
+		for (int i=0; i<poker.length; i++) {
+			int temp = (int)(Math.random()*52);
+			
+			// 檢查機制
+			boolean isRepeat = false;
+			for (int j=0; j<i; j++) {
+				if (poker[j] == temp) {
+					// 重複了
+					isRepeat = true;
+					break;
+				}
+			}
+			
+			if (!isRepeat) {
+				poker[i] = temp;
+				//System.out.println(poker[i]);
+			}else {
+				i--;
+			}
+		}
+		long end = System.currentTimeMillis();
+		System.out.println(end);
+		
+		// 2. 發牌
+		
+		// 3. 攤牌 + 里牌
 	}
-
 }
