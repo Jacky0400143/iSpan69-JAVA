8bd17cd5960d69cd600ee74bde94cd655092939b
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-16 11:53:08 +0800

    2306161153_JW24

---
+++ JWJava/src/tw/org/iii/jwapp/JW24.java
@@ -0,0 +1,13 @@
+package tw.org.iii.jwapp;
+
+import javax.swing.JFrame;
+
+public class JW24 extends JFrame{
+	public JW24() {
+
+	}
+	public static void main(String[] args) {
+		new JW24();
+	}
+
+}
