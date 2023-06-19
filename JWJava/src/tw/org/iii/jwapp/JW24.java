72340414f52114626d890d0ea1abe6d50300b805
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-19 09:55:39 +0800

    230619-0955

--- JWJava/src/tw/org/iii/jwapp/JW24.java
+++ JWJava/src/tw/org/iii/jwapp/JW24.java
@@ -4,10 +4,13 @@ import javax.swing.JFrame;
 
 public class JW24 extends JFrame{
 	public JW24() {
-
+		super("My Windows"); //視窗標題
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
-		new JW24();
+		new JW24(); //呼叫才需要宣告
 	}
 
 }
