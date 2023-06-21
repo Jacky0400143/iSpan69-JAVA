9b7792c377cd8503909f99fb8534c34793719376
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-21 09:17:57 +0800

    0620+1-0917

---
+++ JWJava/src/tw/org/iii/jwapp/GuessNumber.java
@@ -0,0 +1,111 @@
+package tw.org.iii.jwapp;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class GuessNumber extends JFrame {
+	private JButton guess;
+	private JTextField input;
+	private JTextArea log;
+	private String answer;
+	private int counter;
+	
+	public GuessNumber() {
+		super("猜數字");
+		
+		guess = new JButton("猜");
+		input = new JTextField();
+		log = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		JPanel top =  new JPanel(new BorderLayout());
+		add(top, BorderLayout.NORTH);
+		top.add(guess, BorderLayout.EAST);
+		top.add(input, BorderLayout.CENTER);
+		
+		add(log, BorderLayout.CENTER);
+		
+//		guess = new JButton("猜"); b1 = new JButton("B1"); b2 = new JButton("B2");
+//		input = new JTextField();
+//		log = new JTextArea();
+//		
+//		
+//		setLayout(new FlowLayout(FlowLayout.RIGHT));
+//		add(guess); add(b1); add(b2);
+		
+		setSize(640, 480);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		initGame();
+	}
+	private void initGame() {
+		answer = createAnswer(3);
+		counter = 0;
+		//System.out.println(answer);
+	}
+	
+	private void guess() {
+		counter++;
+		String g = input.getText();
+		String result = checkAB(g);
+		log.append(String.format("%d. %s => %s\n", counter, g, result));
+		input.setText("");
+		
+		if (g.equals(answer)) {
+			JOptionPane.showMessageDialog(null, "恭喜");
+		}else if (counter == 10) {
+			JOptionPane.showMessageDialog(null, "Loser: answer:" + answer);
+		}
+		
+	}
+	
+	private String checkAB(String g) {
+		int A, B; A = B = 0;
+		for (int i=0; i<answer.length(); i++) {
+			char c = g.charAt(i);
+			if (c  == answer.charAt(i)) {
+				A++;
+			}else if (answer.indexOf(c) >= 0) {
+				B++;
+			}
+		}
+		return String.format("%dA%dB", A, B);
+	}
+	
+	private static String createAnswer(int d) {
+		int num = 10;
+		int[] poker = new int[num];
+		for (int i=0; i<poker.length; i++) poker[i] = i;
+		
+		for (int i= num-1 ; i > 0; i--) {
+			int rand = (int)(Math.random()*(i+1));
+			// poker[rand] <=> poker[i]
+			int temp = poker[rand];
+			poker[rand] = poker[i];
+			poker[i] = temp;
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for (int i=0; i<d; i++) {
+			sb.append(poker[i]);	
+		}
+		
+		return sb.toString();
+	}
+	
+	public static void main(String[] args) {
+		new GuessNumber();
+	}
+
+}
--- JWJava/src/tw/org/iii/jwapp/JW35.java
+++ JWJava/src/tw/org/iii/jwapp/JW35.java
@@ -3,19 +3,41 @@ package tw.org.iii.jwapp;
 public class JW35 {
 
 	public static void main(String[] args) {
-		Shape s1 = new RecShape(10 ,4);
-		Shape s2 = new CircleShape(3);
+		Shape s1 = new RecShap(10, 4);
+		Shape s2 = new CircleShae(3);
 		System.out.println(s1.calArea());
 		System.out.println(s2.calArea());
 		sumArea(s1, s2);
 	}
-	void
+	
+	static void sumArea(Shape s1, Shape s2) {
+		System.out.println(s1.calArea() + s2.calArea());
+	}
 }
 abstract class Shape {
 	abstract double calArea();
 	abstract double calLength();
 }
-class RecShape extends Shape {
+class RecShap extends Shape{
 	double w, h;
-	RecShape(double w,)
+	RecShap(double w, double h){
+		this.w = w; 
+		this.h = h;
+	}
+	double calArea() {
+		return w*h;
+	}
+	double calLength() {
+		return (w+h)*2;
+	}
+}
+class CircleShae extends Shape {
+	double r;
+	CircleShae(double r){this.r = r;}
+	double calArea() {
+		return Math.PI * r * r;
+	}
+	double calLength() {
+		return 2*r*Math.PI;
+	}	
 }
