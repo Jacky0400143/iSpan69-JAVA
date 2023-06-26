433b5b48dfb94563ed8aaf5118a28ba51ba121df
Jacky0400143 <Jacky0400143+Github@gmail.com>
2023-06-26 11:37:57 +0800

    230626_1135

---
+++ JWJava/src/tw/org/iii/jwapp/JW42.java
@@ -0,0 +1,12 @@
+package tw.org.iii.jwapp;
+
+import java.io.File;
+
+public class JW42 {
+
+	public static void main(String[] args) {
+		System.out.println(File.pathSeparator);
+		System.out.println(File.separator);
+	}
+
+}
